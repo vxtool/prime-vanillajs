153a2e0263a254504134389cd5dbd200a2ff1a36
Hemerson Vianna <hemerson.lourenco@b2wdigital.com>
2016-11-14 18:43:43 -0200

    [add] get query variable

---
+++ source/utils/url.js
@@ -0,0 +1,19 @@
+/**
+ * Get Query Variable
+ *
+ * @param {string} variable
+ * @returns {string}
+ *
+ */
+export function getQueryVariable(variable) {
+  const query = window.location.search.substring(1);
+  const vars = query.split("&");
+
+  for (const i = 0, len = vars.length; i < len; i++) {
+    let pair = vars[i].split("=");
+    if(pair[0] == variable){
+      return pair[1];
+    }
+  }
+  return false;
+}
