86808520a016a415c2df4315fbeb7ffbeec5886d
Hemerson Vianna <hemerson.lourenco@b2wdigital.com>
2016-11-14 18:58:56 -0200

    [update] file of get query variable

--- source/utils/uri.js
+++ source/utils/uri.js
@@ -9,9 +9,9 @@ export function getQueryVariable(variable) {
   const query = window.location.search.substring(1);
   const vars = query.split("&");
 
-  for (const i = 0, len = vars.length; i < len; i++) {
+  for (let i = 0, len = vars.length; i < len; i++) {
     let pair = vars[i].split("=");
-    if(pair[0] == variable){
+    if(pair[0] === variable) {
       return pair[1];
     }
   }
