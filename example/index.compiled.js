37d8ec4b6b3e37d77aa212472159e8f9ca2ea815
Hemerson Vianna <hemerson.lourenco@gmail.com>
2020-07-13 08:56:14 -0300

    feat: add example

--- example/index.compiled.js
+++ example/index.compiled.js
@@ -6,8 +6,13 @@ var _price = require("../lib/components/product/price");
 
 // import makeRequest from '../lib/utils/request';
 var $element = document.querySelector("body");
-(0, _manipulation.addClass)($element, "test");
-(0, _manipulation.hasClass)($element, "test");
-(0, _manipulation.removeClass)($element, "test"); // makeRequest({ url: '/' }, () => {});
+(0, _manipulation.addClass)($element, "test1");
+(0, _manipulation.addClass)($element, "test2");
 
-(0, _price.priceUnformat)("R$ 123,00");
+if ((0, _manipulation.hasClass)($element, "test1")) {
+  console.log("has class test1");
+}
+
+(0, _manipulation.removeClass)($element, "test1"); // makeRequest({ url: '/' }, () => {});
+
+document.querySelector('[data-js="main"]').innerHTML = (0, _price.priceUnformat)("R$ 123,00");
--- example/index.js
+++ example/index.js
@@ -4,10 +4,15 @@ import { priceUnformat } from "../lib/components/product/price";
 
 const $element = document.querySelector("body");
 
-addClass($element, "test");
-hasClass($element, "test");
-removeClass($element, "test");
+addClass($element, "test1");
+addClass($element, "test2");
+if (hasClass($element, "test1")) {
+  console.log("has class test1");
+}
+removeClass($element, "test1");
 
 // makeRequest({ url: '/' }, () => {});
 
-priceUnformat("R$ 123,00");
+document.querySelector('[data-js="main"]').innerHTML = priceUnformat(
+  "R$ 123,00"
+);
